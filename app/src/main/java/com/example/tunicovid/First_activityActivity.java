6e3df9605a7c2f81127572ae7e59fd0b5efceab5
malek chiha <malekchia75@gmail.com>
2020-12-29 11:46:21 +0100

    Connect pages

--- app/src/main/java/com/example/tunicovid/First_activityActivity.java
+++ app/src/main/java/com/example/tunicovid/First_activityActivity.java
@@ -2,13 +2,31 @@ package com.example.tunicovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class First_activityActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
+        btn = findViewById(R.id.buttontest); btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(First_activityActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
+        btn = findViewById(R.id.btn_propagation); btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(First_activityActivity.this,MainActivity8.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/tunicovid/MainActivity3.java
+++ app/src/main/java/com/example/tunicovid/MainActivity3.java
@@ -13,7 +13,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        btn = findViewById(R.id.button4); btn.setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.button_next2); btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity3.this,MainActivity4.class);
